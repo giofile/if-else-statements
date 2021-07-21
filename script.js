c11678fcf5305f53059aa5ee464c9db7140f29b6
giofile <77681874+giofile@users.noreply.github.com>
2021-07-21 14:10:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const age = 20
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18 ){
+ console.log("u mag een drankje komen benuttigen");
+} else {
+    console.log("helaas, vanaf uw 18e jaar,  bent u zeer welkom ");
+}
+
+if( isFemale === true){
+    console.log("Goedendag , welkom");
+} else{
+    console.log("Helaas, enkel dames vandaag");
+}
+
+
+if( driverStatus ==="bob"){
+    console.log("u mag rijden");
+} else{
+    console.log("u mag niet rijden");
+}
